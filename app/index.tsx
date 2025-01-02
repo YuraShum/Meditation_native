d6c46e9dddaa6780873e12a0e9a5216c69a63b5f
YuraShum <91155294+YuraShum@users.noreply.github.com>
2025-01-02 13:54:27 +0200

    feat: added nativeWind v4 to project

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
 import { View, Text } from "react-native";
+import "../global.css";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text style={{ margin: 40, color: "black" }}>sgsdf</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-4xl text-orange-400 ">sgsdf</Text>
     </View>
   );
 }
