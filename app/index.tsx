0bd848c51205201d6e84f03e07ad136116a9667d
YuraShum <91155294+YuraShum@users.noreply.github.com>
2025-01-02 13:13:37 +0200

    feat: add static assets and define constant files

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text style={{ margin: 40, color: "black" }}>sgsdf</Text>
+    </View>
+  );
+}
