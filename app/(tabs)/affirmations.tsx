fa9175676feacf9dfe32575cab356b3d32df6982
YuraShum <91155294+YuraShum@users.noreply.github.com>
2025-01-03 22:36:22 +0200

    feat: created tabs layout with tabs screen

---
+++ app/(tabs)/affirmations.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+type Props = {};
+
+const Affirmations = (props: Props) => {
+  return (
+    <View>
+      <Text>affirmations</Text>
+    </View>
+  );
+};
+
+export default Affirmations;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,21 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-    return (
-        <Slot/>
-    )
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
