30ff1cccd990a229e9596b8b271d0b64de4193f7
YuraShum <91155294+YuraShum@users.noreply.github.com>
2025-01-06 14:26:09 +0200

    feat: created utilities for time conversion

---
+++ utils/formattedTime.ts
@@ -0,0 +1,7 @@
+export const formattedTimeMinutes = (seconds: number) => {
+    return String(Math.floor(seconds / 60)).padStart(2, '0');
+}
+
+export const formattedTimeSeconds = (seconds: number) => {
+    return String(seconds % 60).padStart(2, '0')
+}
